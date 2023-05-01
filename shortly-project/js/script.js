ef3149180b8347507dccb4c3ed06784d89175f01
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2023-05-01 19:58:22 +0500

    Finished validation of shortly project

---
+++ shortly-project/js/script.js
@@ -0,0 +1,34 @@
+const linkForm = document.getElementById('link-form');
+const input = document.getElementById('link-input');
+const errorMessage = document.getElementById('error-message');
+
+linkForm.addEventListener('submit', handleSubmit);
+
+function validURL(str) {
+  var pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+    '((\\d{1,3}\\.){3}\\d{1,3}))' +
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+    '(\\?[;&a-z\\d%_.~+=-]*)?' +
+    '(\\#[-a-z\\d_]*)?$',
+    'i'
+  )
+  return !!pattern.test(str)
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  if (input.value === "") {
+    input.classList.add('border-red-500');
+    errorMessage.innerHTML = "Please enter something";
+  } else if (!validURL(input.value)) {
+    input.classList.add('border-red-500');
+    errorMessage.innerHTML = "Please enter a valid URL";
+  } else {
+    input.classList.remove('border-red-500');
+    errorMessage.innerHTML = "";
+    alert('success');
+  }
+}
